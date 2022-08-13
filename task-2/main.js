6aa36d8d92931643edefae434e17a39a54234f5b
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-13 13:53:37 +0400

    Task 2 done

--- task-2/main.js
+++ task-2/main.js
@@ -9,13 +9,15 @@ prompt.get('input', function (err, result) {
     let a = parseInt(numbers[0])
     let b = parseInt(numbers[1])
     let c = parseInt(numbers[2])
-    if (a,b,c<10**9) {
-        if (a>b&&a<c) {
+    if (a<10**9&&b<10**9&&c<10**9) {
+        if (a>=b&&a<=c) {
             console.log("IN");
         } else {
             console.log("OUT");
         }
-    } 
+    } else {
+        console.log("ERROR");
+    }
     
   
 });
