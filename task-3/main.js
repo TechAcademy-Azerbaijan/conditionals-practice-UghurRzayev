f99bb21596254b8e85029febed4d51969f37503d
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-13 14:10:01 +0400

    Task 3  done

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let input = result.input
+    let numbers = input.split(',')
+    let a = parseInt(numbers[0])
+    let b = parseInt(numbers[1])
+    let c = parseInt(numbers[2])
+    let d = parseInt(a+b)
+    let e = parseInt(a+c)
+    let f = parseInt(b+c)
+    if (d>c&&e>b&&f>a) {
+        console.log("YES");
+    } else {
+        console.log("NO");
+    }
   
 });
