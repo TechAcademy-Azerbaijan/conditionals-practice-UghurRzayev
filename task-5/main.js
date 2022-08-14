a8f4e9b8e591299ce9327716b896b887129812e5
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-14 08:30:51 +0400

    Task 5  done

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = parseInt(result.input)
+    let b = parseInt(a/100)
+    let c = parseInt(a%1000)
+    let d = parseInt(c/10)
+    let e = parseInt(a%100)
+    if (b==37||d==37||e==37) {
+        console.log("YES");
+    } else {
+        console.log("NO");
+    }
   
 });
