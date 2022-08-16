6fa416fcb84eb37ef9125befae2022c8422a1219
Ugur Rzayev <ughurrzayev@gmail.com>
2022-08-16 10:35:58 +0400

    Task 1 done

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = parseInt(result.input)
+    let b = parseInt(a**3+a*5)
+    let c = parseInt(a**2-2*a+4)
+    if (-10000 <= a&&a <= 10000) {
+        if (a>=10) {
+            console.log(b);
+        } else if (a<10) {
+            console.log(c);
+        }
+    } else {
+        console.log("ERROR");
+    }
   
 });
